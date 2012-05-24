0bd4842ced8b5f5345726ad9a6e96e8d46761fab
xiasiyu <xiasiyu@gmail.com>
2012-05-24 15:04:01 +0800

    Finish edit,update,index,destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only:[:edit, :update]
+  before_filter :admin_user, only: :destroy
+
   def show
     @user = User.find( params[ :id ] )
   end
+
   def new
     @user = User.new
   end
@@ -31,4 +36,33 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def index
+  @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy 
+    User.find( params[:id]).destroy
+	flash[:success] = "User destroyed"
+	redirect_to users_path
+  end
+
+private
+  def signed_in_user
+    unless signed_in?
+	store_location
+    redirect_to signin_path, notice: "Please sign in."
+  end
+
+  def correct_user
+    @user = User.find( params[:id] )
+    redirect_to( root_path ) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+
+
+end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,7 @@
 include ApplicationHelper
 
 
-def valid_signin( user )
+def sign_in( user )
   visit signin_path
   fill_in "Email", with: user.email
   fill_in "Password", with:user.password
